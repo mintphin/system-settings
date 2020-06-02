69c2bf981fbd708448d9d9911b2fd991d3a16c66
mintphin <meltstrap@uiiverse.xyz>
2020-06-01 23:15:05 -0400

    Modified model properties for compatibility with GSI

--- plugins/about/storageabout.cpp
+++ plugins/about/storageabout.cpp
@@ -172,8 +172,8 @@ QString StorageAbout::vendorString()
     {
         char manufacturerBuffer[PROP_VALUE_MAX];
         char modelBuffer[PROP_VALUE_MAX];
-        property_get("ro.product.manufacturer", manufacturerBuffer, "");
-        property_get("ro.product.model", modelBuffer, "");
+        property_get("ro.product.vendor.manufacturer", manufacturerBuffer, "");
+        property_get("ro.product.vendor.model", modelBuffer, "");
         m_vendorString = QString("%1 %2").arg(manufacturerBuffer).arg(modelBuffer);
     }
 
